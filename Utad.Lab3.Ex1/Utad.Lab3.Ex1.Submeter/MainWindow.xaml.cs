4ef26b1d6ce6957dc7a50785fa339237e4adb4f0
SimaoLopes9 <101218375+SimaoLopes9@users.noreply.github.com>
2022-03-08 21:14:06 +0000

    Add files via upload

---
+++ Utad.Lab3.Ex1/Utad.Lab3.Ex1.Submeter/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Utad.Lab3.Ex1.Submeter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int s1= Int32.Parse(soma1tb.Text);
+            int s2= Int32.Parse(soma2tb.Text);
+            calcularSoma.Text =  (s1+s2).ToString();
+        }
+    }
+}
